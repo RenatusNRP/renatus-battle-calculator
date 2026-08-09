c78e0aec874e3a3e9912eba142a35d3906bca730
marsasgrg <92935256+marsasgrg@users.noreply.github.com>
2026-08-09 20:17:24 +0300

    Color units

--- Assets/Scripts/MilitaryUnit.cs
+++ Assets/Scripts/MilitaryUnit.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 public class MilitaryUnit : NetworkBehaviour
 {
     public NetworkVariable<NetworkObjectReference> owningPlayer;
-    
+    public void Update()
+    {
+
+        if (owningPlayer.Value.TryGet(out NetworkObject owner) && owner == NetworkManager.Singleton.LocalClient.PlayerObject)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
+        }
+        else gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+    }
 }
 
